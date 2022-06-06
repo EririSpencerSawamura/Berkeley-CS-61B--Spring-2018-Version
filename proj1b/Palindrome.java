2522da667da9db33986440c51bb5c3f1a06a2c65
Ren Wang <renwanginmath@gmail.com>
2022-06-06 14:29:12 +0800

    Updated

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -2,7 +2,7 @@ public class Palindrome {
     /** Returns a Deque where the characters appear in the same order as in the String. */
     public Deque<Character> wordToDeque(String word) {
         Deque<Character> dq = new LinkedListDeque<>();
-        for (int i =0; i < word.length(); i++) {
+        for (int i = 0; i < word.length(); i++) {
             dq.addLast(word.charAt(i));
         }
         return dq;
@@ -27,7 +27,7 @@ public class Palindrome {
     /** Overloaded method of the above. Returns true if the given word
      * is a palindrome with regard to the "Off-By-One" rule for
      * identical characters, and false otherwise. */
-    public boolean isPalindrome(String word, OffByOne obo) {
+    public boolean isPalindrome(String word, CharacterComparator cc) {
         if (word == null || word.length() <= 1) {
             return true;
         }
@@ -35,7 +35,7 @@ public class Palindrome {
         while (dq.size() > 1) {
             char first = dq.removeFirst();
             char last = dq.removeLast();
-            if (!obo.equalChars(first, last)) {
+            if (!cc.equalChars(first, last)) {
                 return false;
             }
         }
